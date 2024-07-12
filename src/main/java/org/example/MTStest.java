db135a629fcb5a6e3794f746ef781b213f915ba4
kZ2507 <160583616+kZ2507@users.noreply.github.com>
2024-07-12 21:48:30 +0300

    Update MTStest.java

--- src/main/java/org/example/MTStest.java
+++ src/main/java/org/example/MTStest.java
@@ -23,33 +23,41 @@ public class MTStest {
 
 
         @Test
+            public void t1 {
         String expectedTitle;
         expectedTitle = "Онлайн пополнение без комиссии";
         String actualText = driver.findElement(By.xpath("//div[@class='pay__wrapper']/h2")).getText().replaceAll("\\n", " ");
         Assertions.assertEquals(expectedTitle, actualText);
-
-
-        @Test
-        List<WebElement> logos = driver.findElements(By.xpath("//div[@class='pay__partners']/descendant::img"));
-        for(int i = 0; i< ((List<?>) logos).size(); i++) {
-            String url = logos.get(i).getAttribute("src");
         }
 
 
         @Test
+            public void t2 {
+        List<WebElement> logos = driver.findElements(By.xpath("//div[@class='pay__partners']/descendant::img"));
+        for(int i = 0; i< ((List<?>) logos).size(); i++) {
+            String url = logos.get(i).getAttribute("src");
+        }
+        }
+
+
+        @Test
+            public void t3 {
         WebElement infoLink = driver.findElement(By.xpath("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
         infoLink.click();
         String expectredURL = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
         String actualURL = driver.getCurrentUrl();
         Assert.assertEquals(actualURL, expectredURL);
+        }
 
 
         @Test
+            public void t4 {
         WebElement buttonSubmit= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//form[@id='pay-connection']")));
         WebElement inputPhone = driver.findElement(By.xpath("//input[@id='connection-phone']"));
         inputPhone.sendKeys("297777777");
         WebElement inputSum = driver.findElement(By.xpath("//input[@id='connection-sum']"));
         inputSum.sendKeys("1");
         buttonSubmit.click();
+        }
     }
 }
